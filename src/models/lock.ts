8dc8b3493494957b828c88d4f0a15dfe828ff329
JRC-Tojo <274191739+JRC-Tojo@users.noreply.github.com>
2026-08-18 10:04:51 +0000

    🚨 Apply Prettier Change

--- src/models/lock.ts
+++ src/models/lock.ts
@@ -9,4 +9,5 @@
  *   const buf = await crypto.subtle.digest('SHA-256', new TextEncoder().encode('決めたパスフレーズ'));
  *   [...new Uint8Array(buf)].map((b) => b.toString(16).padStart(2, '0')).join('');
  */
-export const LOCK_PASSWORD_HASH = 'a2db139e00a59a7b86fb22f65c59a897fee15bc1c43b33e6b3feb734647e5146';
+export const LOCK_PASSWORD_HASH =
+  'a2db139e00a59a7b86fb22f65c59a897fee15bc1c43b33e6b3feb734647e5146';
